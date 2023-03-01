7e8b57fb090f66a300b8aa398cd25a0809689b6c
Alejandro <alexportalescervero@gmail.com>
2023-03-01 13:07:32 +0100

    Tag filter - Video 9

--- resources/views/posts/tag.blade.php
+++ resources/views/posts/tag.blade.php
@@ -2,7 +2,7 @@
 
     <div class="mx-auto max-w-5xl px-2 sm:px-6 lg:px-8 py-8">
 
-        <h1 class="uppercase text-center text-3xl font-bold mb-8">Category: {{$category->name}}</h1>
+        <h1 class="uppercase text-center text-3xl font-bold mb-8">Tag: {{$tag->name}}</h1>
 
         @foreach ($posts as $post)
         
