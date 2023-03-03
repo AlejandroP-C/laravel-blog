0ad5972f499725a302d5977d4770708f89b09a43
Alejandro <alexportalescervero@gmail.com>
2023-03-03 12:37:52 +0100

    URL protection depending on user roles - Video 24

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -12,6 +12,14 @@ use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
+
+    public function __construct() {
+        $this->middleware('can:admin.posts.index')->only('index');
+        $this->middleware('can:admin.posts.create')->only('create', 'store');
+        $this->middleware('can:admin.posts.edit')->only('edit','update');
+        $this->middleware('can:admin.posts.destroy')->only('destroy');
+    }
+
     /**
      * Display a listing of the resource.
      */
@@ -56,14 +64,6 @@ class PostController extends Controller
 
         return redirect()->route('admin.posts.edit', $post)->with('info', 'El post se creó con éxito');
 
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Request $request)
-    {
-        //
     }
 
     /**
--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -8,6 +8,14 @@ use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
+
+    public function __construct() {
+        $this->middleware('can:admin.tags.index')->only('index');
+        $this->middleware('can:admin.tags.create')->only('create', 'store');
+        $this->middleware('can:admin.tags.edit')->only('edit','update');
+        $this->middleware('can:admin.tags.destroy')->only('destroy');
+    }
+
     /**
      * Display a listing of the resource.
      */
@@ -56,14 +64,6 @@ class TagController extends Controller
         return redirect()->route('admin.tags.edit', compact('tag'))->with('info', 'La etiqueta se creó con éxito');
 
 
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Tag $tag)
-    {
-        //
     }
 
     /**
